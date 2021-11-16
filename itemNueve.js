cbcc7780bbd64210228927a2042fe8c468dfbf39
synti <synti.bogado@gmail.com>
2021-11-16 17:48:33 -0300

    otra modif

--- itemNueve.js
+++ itemNueve.js
@@ -1,5 +1,7 @@
+console.log('*******************');
 console.log('hello! Good tuesday');
 console.log('the storm is coming!');
-console.log('*********************');
+console.log('********************');
+console.log('********************');
 
 
